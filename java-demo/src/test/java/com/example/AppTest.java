7c853288cd36f51a6a68a4f67c83746f64a08abc
Kalel <12826@intelizign.com>
2025-04-15 15:05:46 +0530

    Added java project

---
+++ java-demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
